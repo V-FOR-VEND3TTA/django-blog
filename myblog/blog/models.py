9d4563f98149ca1bc025bbb45c6ed33e2303783f
George Hlongwane <georgehlongwane8@gmail.com>
2023-10-03 09:07:46 +0200

    Added blog model and registered it into our admin

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Custom models
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
