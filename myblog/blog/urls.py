e9f2ee9feb9ad8cb42866b7ad4aa6e046337e4fa
VidyaYadavv1519 <vidyayadav617@gmail.com>
2023-10-15 20:11:34 +0530

    created user authentication

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+    
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 def post_list(request):
     posts = Post.objects.all()
@@ -8,5 +11,16 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('post_list') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
 
 
